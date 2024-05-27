24a60eee660b41f8c1b5d5c0d4e613042f51e42b
Mohamad Khaefi <mkhaefi2500@yahoo.com>
2024-05-27 16:21:38 +0330

    Finish Adding Blog List Item Animations

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -4,6 +4,9 @@ import { asImageSrc, Content, isFilled } from "@prismicio/client";
 import { MdArrowOutward } from "react-icons/md";
 import Link from "next/link";
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 type ContentListProps = {
   items: Content.BlogPostDocument[] | Content.ProjectDocument[];
@@ -20,6 +23,7 @@ const ContentList = ({
   const component = useRef(null);
   const revealRef = useRef(null);
   const [currentItem, setCurrentItem] = useState<null | number>(null);
+  const itemsRef = useRef<Array<HTMLLIElement | null>>([]);
 
   const contentImages = items.map((item) => {
     const image = isFilled.image(item.data.hover_image)
@@ -33,6 +37,7 @@ const ContentList = ({
       exp: -10,
     });
   });
+
   const onMouseEnter = (index: number) => {
     setCurrentItem(index);
   };
@@ -42,6 +47,34 @@ const ContentList = ({
   const lastMousePos = useRef({ x: 0, y: 0 });
 
   useEffect(() => {
+    let ctx = gsap.context(() => {
+      itemsRef.current.forEach((item) => {
+        gsap.fromTo(
+          item,
+          {
+            opacity: 0,
+            y: 120,
+          },
+          {
+            scale: 1,
+            y: 0,
+            opacity: 1,
+            duration: 1.3,
+            ease: "elastic.out(1,0.3)",
+            stagger: 0.2,
+            scrollTrigger: {
+              trigger: item,
+              start: "top bottom-=300px",
+              end: "bottom center",
+              toggleActions: "play none none none",
+            },
+          },
+        );
+      });
+    });
+  }, []);
+
+  useEffect(() => {
     const handleMouseMove = (e: MouseEvent) => {
       const mousePos = {
         x: e.clientX,
@@ -49,18 +82,22 @@ const ContentList = ({
       };
 
       //   Calculate Mouse Speed And Direction
-      const speed = Math.sqrt(Math.pow(mousePos.x - lastMousePos.current.x, 2));
+      const speed = Math.sqrt(
+        Math.pow(mousePos.x - lastMousePos.current.x, 2) +
+          Math.pow(mousePos.y - lastMousePos.current.y, 2),
+      );
 
       let ctx = gsap.context(() => {
         if (currentItem !== null) {
           const maxY = window.scrollY + window.innerHeight - 350;
-          const maxX = window.innerHeight - 250;
+          const maxX = window.innerWidth + 250;
           gsap.to(revealRef.current, {
             x: gsap.utils.clamp(0, maxX, mousePos.x - 110),
             y: gsap.utils.clamp(0, maxY, mousePos.y - 160),
             rotation: speed * (mousePos.x > lastMousePos.current.x ? 1 : -1),
-            ease: "back.out(2)",
+            ease: "back.out(5)",
             duration: 1.3,
+            opacity: 0.7,
           });
         }
         lastMousePos.current = mousePos;
@@ -87,8 +124,9 @@ const ContentList = ({
             {isFilled.keyText(item.data.title) && (
               <li
                 key={index}
-                className="list-item opacity-0f"
+                className="list-item opacity-0"
                 onMouseEnter={() => onMouseEnter(index)}
+                ref={(e) => (itemsRef.current[index] = e)}
               >
                 <Link
                   href={urlPreFix + "/" + item.uid}
@@ -114,10 +152,10 @@ const ContentList = ({
           </>
         ))}
       </ul>
-      {/*Hover Element*/}
+      {/*Hover Image Element*/}
       <div
         ref={revealRef}
-        className="hover-reveal pointer-events-auto absolute left-0 top-0 -z-10 h-[320px] w-[250px] rounded-lg bg-cover bg-center opacity-0f transition-[background] duration-300"
+        className="hover-reveal pointer-events-auto absolute left-0 top-0 -z-10 h-[320px] w-[250px] rounded-lg bg-cover bg-center opacity-0 transition-[background] duration-300"
         style={{
           backgroundImage:
             currentItem !== null ? `url(${contentImages[currentItem]}})` : " ",
